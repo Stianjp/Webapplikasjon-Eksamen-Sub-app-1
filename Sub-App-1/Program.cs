774652d161375c83a23d2762986be640a2433fb1
Magnus <s375129@oslomet.no>
2024-10-08 14:32:05 +0200

    Implemented secure cookie changes
    Subject to change. Seems to work after basic system testing.

--- Sub-App-1/Program.cs
+++ Sub-App-1/Program.cs
@@ -12,8 +12,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite
 
 // Add cookie authentification (Todo: test)
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
-    options.LoginPath = "/Account/Index"; // Login path - where to redirect if authentication is needed
+    options.LoginPath = "/Account/Index"; // Login path - where to redirect if authentication is a success
     options.AccessDeniedPath = "/Account/AccessDenied"; // TODO: Access denied path - where to redirect if authorization fails
+
+    options.Cookie.HttpOnly = true; // prevents client-side scripts from accessing the cookie. Security against XSS attacks.
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // cookie only travels accoss HTTPS.
+    options.Cookie.SameSite = SameSiteMode.Strict; // prevents the cookie from being sent in cross-site requests. Security against CSRF.
+    options.Cookie.Name = "SubApp1_Auth_Cookie"; // todo: Unique cookie name subject to change (or are we calling it this?)
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // how long should the user stay logged in.
+    options.SlidingExpiration = true; // if the user interacts with the server in this timespan, the timer refreshes so the user stays logged in.
 });
 
 var app = builder.Build();
