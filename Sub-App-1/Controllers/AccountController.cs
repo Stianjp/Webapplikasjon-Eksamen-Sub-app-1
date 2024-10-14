060600e49ce5a66a0fe5feb2a39b59515e25a04c
NazliQ <nazlipikulser@gmail.com>
2024-10-14 15:31:31 +0200

    Klarte å overkjøre endringer fra branch til stiantest, det skal nå være lagt inn i main. Lagt til noen ekstraknapper som testing, de fungrer ikke enda. Endrett et par ting i overlay-not-logged inn, fortsatt behow for endringer

--- Sub-App-1/Controllers/AccountController.cs
+++ Sub-App-1/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ public class AccountController : Controller {
     }
 
     // /Account/Index
-    public IActionResult Accountindex() {
+    public IActionResult Index() {
         return View();
     }
 
