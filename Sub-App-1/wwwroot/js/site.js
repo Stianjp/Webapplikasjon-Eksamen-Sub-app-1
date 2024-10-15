07b79164a8ae749654c5ba01c507b9b474b24205
Stianjp <114061998+Stianjp@users.noreply.github.com>
2024-10-15 10:26:52 +0200

    Active sidebar
    Gjør at den siden man er på blir grønn i sidebar

--- Sub-App-1/wwwroot/js/site.js
+++ Sub-App-1/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/* To make the sidebar green for active site */
+document.addEventListener("DOMContentLoaded", function () {
+    var currentUrl = window.location.pathname;
+    var navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(function (link) {
+        if (link.getAttribute("href") === currentUrl) {
+            link.classList.add("bg-success", "text-light", "active");
+        }
+    });
+});
