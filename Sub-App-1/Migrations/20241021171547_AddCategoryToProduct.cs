59d078cdd0ece9132f3a7f7a054ea8717b57bf50
NazliQ <nazlipikulser@gmail.com>
2024-10-21 19:17:03 +0200

    Migration av en ny variabel, category i db og model

---
+++ Sub-App-1/Migrations/20241021171547_AddCategoryToProduct.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sub_App_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_ProducerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProducerId",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_ProducerId",
+                table: "Products",
+                column: "ProducerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_ProducerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProducerId",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_ProducerId",
+                table: "Products",
+                column: "ProducerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
