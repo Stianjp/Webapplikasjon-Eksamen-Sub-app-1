05f3471e60c5f7cddba84b01568416ca78c55a74
Stianjp <114061998+Stianjp@users.noreply.github.com>
2024-10-21 17:23:39 +0200

    Legger til */

--- Sub-App-1/Controllers/FoodProducerController.cs
+++ Sub-App-1/Controllers/FoodProducerController.cs
@@ -5,13 +5,13 @@ using Sub_App_1.Models;  // For Product-modellen
 using System.Security.Claims;  // For ClaimTypes og User.FindFirstValue
 using Microsoft.AspNetCore.Authorization;
 
-@* TODO 
+/*@* TODO 
 *Implementer GetProducerData() for å hente produkter
 *Dobbel sjekk av model sine attributter passer med visningen i view dashboard cards
 * Omdiriger til ProductsController sin Create-aksjon til FoodProducerController sin lag nytt produkt
 for å gjennombruk db og metoder, da de skal gjøre det samme både i produktsiden og dashboard. 
 *Metoderw vil da være create, edit og delete et Product.
- *@
+ *@ */
 
 [Authorize(Roles = "FoodProducer")]
 public class FoodProducerController : Controller
