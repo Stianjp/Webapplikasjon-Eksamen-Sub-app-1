a49ef04d5e9de4bd113483849b509234724aaf69
NazliQ <nazlipikulser@gmail.com>
2024-10-21 18:59:22 +0200

    Changes to be committed:     modified:   Sub-App-1/Models/Product.cs     modified:   Sub-App-1/Views/FoodProducer/ProducerDashboard.cshtml     modified:   Sub-App-1/Views/Products/Productsindex.cshtml     modified:   Sub-App-1/Views/Shared/_Layout.cshtml     modified:   Sub-App-1/wwwroot/css/site.css

--- Sub-App-1/Models/Product.cs
+++ Sub-App-1/Models/Product.cs
@@ -29,6 +29,8 @@ public class Product
     [Required]
     public double Fat { get; set; } // grams per 100g
 
+    public string Category {get; set; }
+
     //Behov for kategorier av type mat
     // public string Category {get; set; } // Skal vi bruke en liste med kategorier som er alt satt av oss?
 
