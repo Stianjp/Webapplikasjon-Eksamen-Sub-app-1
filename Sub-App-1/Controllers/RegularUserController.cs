8051c951d7cda2a14c91775b90f4e9d94dea92de
Magnus <s375129@oslomet.no>
2024-10-24 17:08:38 +0200

    Fixed RegularUserController
    Oh my.. That took too long to discover..

--- Sub-App-1/Controllers/RegularUserController.cs
+++ Sub-App-1/Controllers/RegularUserController.cs
@@ -2,10 +2,10 @@ namespace Sub_App_1.Controllers;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sub_App_1.Models;
 
 [Authorize(Roles = UserRoles.RegularUser + "," + UserRoles.Administrator)]
 public class RegularUserController : Controller {
-    [Route("RegularUser/Dashboard")]
     public IActionResult Dashboard() {
         return View();
     }
